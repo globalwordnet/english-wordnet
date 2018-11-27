2fe8a4fd337fc03571d01ca9bc95634724534e55
John P. McCrae <john@mccr.ae>
2018-11-27 14:32:50 +0000

    Fix #33
    Update the individual lexicographer files to contain a number to
    indicate the order of senses, this is then discarded by the `merge.py`
    script

--- scripts/merge.py
+++ scripts/merge.py
@@ -12,19 +12,53 @@ def merge_entry(e1, e2):
             i += 1
     return e1
 
+def order_entry(e):
+    f = ET.Element('LexicalEntry')
+    f.attrib = e.attrib
+    senses = []
+    for c in e:
+        if c.tag == "Lemma" or c.tag == "Form":
+            f.append(c)
+        elif c.tag == "Sense":
+            senses.append(c)
+    senses.sort(key=lambda x: int(x.attrib["n"]))
+    for c in senses:
+        del c.attrib["n"]
+        f.append(c)
+    for c in e:
+        if c.tag == "SyntacticBehaviour":
+            f.append(c)
+    return f
+
+def indent(elem, level=0):
+    i = "\n" + level*"  "
+    j = "\n" + (level+1)*"  "
+    if len(elem):
+        elem.text = j
+        n = 1
+        for subelem in elem:
+            if n != len(elem):
+                indent(subelem, level+1)
+                subelem.tail = j
+            else:
+                subelem.tail = i
+            n += 1
+    else:
+        elem.tail = i
+    return elem  
 
 def main():
     with open("wn31.xml", "w") as out:
         out.write("""<?xml version="1.0" encoding="UTF-8"?>
-    <!DOCTYPE LexicalResource SYSTEM "http://globalwordnet.github.io/schemas/WN-LMF-1.0.dtd">
-    <LexicalResource xmlns:dc="http://purl.org/dc/elements/1.1/">
-      <Lexicon id="ewn" 
-               label="English WordNet"
-               language="en"
-               email="john@mccr.ae"
-               license="https://wordnet.princeton.edu/license-and-commercial-use"
-               version="3.1" 
-               url="https://github.com/globalwordnet/english-wordnet">""")
+<!DOCTYPE LexicalResource SYSTEM "http://globalwordnet.github.io/schemas/WN-LMF-1.0.dtd">
+<LexicalResource xmlns:dc="http://purl.org/dc/elements/1.1/">
+  <Lexicon id="ewn" 
+           label="English WordNet"
+           language="en"
+           email="john@mccr.ae"
+           license="https://wordnet.princeton.edu/license-and-commercial-use"
+           version="3.1" 
+           url="https://github.com/globalwordnet/english-wordnet">""")
         lex_entries = {}
 
         for wn31_part in glob("src/wn31-*.xml"):
@@ -37,8 +71,10 @@ def main():
                     else:
                         lex_entries[id] = element
         for (k, e) in lex_entries.items():
-            out.write(ET.tostring(e).decode()
+            out.write("\n    ")
+            out.write(ET.tostring(indent(order_entry(e),level=2)).decode()
                     .replace(" xmlns:dc=\"http://purl.org/dc/elements/1.1/\"",""))
+        out.write("\n    ")
 
         for wn31_part in glob("src/wn31-*.xml"):
             tree = ET.parse(wn31_part).getroot()
