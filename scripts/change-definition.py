cd60bba04ed157a266be2765e6545f28a3469764
John P. McCrae <john@mccr.ae>
2019-11-13 09:25:06 +0000

    Debug change definition

--- scripts/change-definition.py
+++ scripts/change-definition.py
@@ -4,24 +4,27 @@ import argparse
 import os
 import pickle
 
-def update_def(wn, synset, defn):
+def update_def(wn, synset, defn, add):
     print("Previous definitions:")
     for d in synset.definitions:
-        print("> " + d)
+        print("> " + d.text)
     wn_synset = wordnet.parse_wordnet("src/wn-%s.xml" % synset.lex_name)
     ss = wn_synset.synset_by_id(synset.id)
-    ss.definitions = [defn]
-    with open("src/wn-%s.xml" % source.lex_name, "w") as out:
-        wn_source.to_xml(out, True)
+    if add:
+        ss.definitions = ss.definitions + [wordnet.Definition(defn)]
+    else:
+        ss.definitions = [wordnet.Definition(defn)]
+    with open("src/wn-%s.xml" % synset.lex_name, "w") as out:
+        wn_synset.to_xml(out, True)
 
 def main():
     parser = argparse.ArgumentParser(description="Change a relationship within the wordnet")
     parser.add_argument('id', metavar='ID', type=str, 
             help="The ID of the synset (sense) for the relationship")
+    parser.add_argument('--add', action='store_true',
+            help="Add the new definition and retain the previous definition (otherwise this definition replaces previous definitions)")
     parser.add_argument('--defn', type=str,
             help="The new definition")
-    parser.add_argumnet('--add',
-            help="Add the new definition and retain the previous definition (otherwise this definition replaces previous definitions)")
 
     args = parser.parse_args()
 
@@ -33,17 +36,17 @@ def main():
     else:
         wn = pickle.load(open("wn.pickle", "rb"))
 
-    synset = wn.synset_by_id(args.source_id)
+    synset = wn.synset_by_id(args.id)
 
     if not synset:
-        print("Could not find the synset %s" % args.source_id)
+        print("Could not find the synset %s" % args.id)
         sys.exit(-1)
 
     if not args.defn:
         print("Please specify a definition")
         sys.exit(-1)
 
-    update_def(wn, synset, args.defn)
+    update_def(wn, synset, args.defn, args.add)
 
 if __name__ == "__main__":
     main()
