4f7d451153c0b165b1c346c67ac5b13e2e5ca72a
John P. McCrae <john@mccr.ae>
2019-11-15 04:16:23 +0000

    Fix bugs in change manager

--- scripts/change_manager.py
+++ scripts/change_manager.py
@@ -37,13 +37,41 @@ def insert_rel(source, rel_type, target):
     with open("src/wn-%s.xml" % source.lex_name, "w") as out:
         wn_source.to_xml(out, True)
 
- 
+def empty_if_none(x):
+    """Returns an empty list if passed None otherwise the argument"""
+    if x:
+        return x
+    else:
+        return []
+
+def escape_lemma(lemma):
+    """Format the lemma so it is valid XML id"""
+    def elc(c):
+        if (c >= 'A' and c <= 'Z') or (c >= 'a' and c <= 'z'):
+            return c
+        elif c == ' ':
+            return '_'
+        elif c == '(':
+            return '-lb-'
+        elif c == ')':
+            return '-rb-'
+        elif c == '\'':
+            return '-ap-'
+        elif c == '/':
+            return '-sl-'
+        else:
+            return '-%04x-' % ord(c)
+
+    return "".join(elc(c) for c in lemma)
+
+def synset_key(synset_id):
+    return synset_id[4:-2]
 
 def add_entry(wn, synset, lemma, idx=0, n=-1):
     """Add a new lemma to a synset"""
     print("Adding %s to synset %s" % (lemma, synset.id))
     wn_synset = parse_wordnet("src/wn-%s.xml" % synset.lex_name)
-    entries = [entry for entry in wn_synset.entry_by_lemma(lemma) if entry.lemma.part_of_speech == synset.part_of_speech]
+    entries = [entry for entry in empty_if_none(wn_synset.entry_by_lemma(lemma)) if entry.lemma.part_of_speech == synset.part_of_speech]
     n_entries = len(wn.members_by_id(synset.id))
     if idx <= 0:
         idx = n_entries + 1
@@ -74,23 +102,23 @@ def add_entry(wn, synset, lemma, idx=0, n=-1):
                     change_sense_n(wn, entry_global, sense.id, sense.n + 1)
 
         entry.senses.append(Sense(
-            id="ewn-%s-%s-%s-%02d" % (escape_lemma(lemma), synset.part_of_speech,
+            id="ewn-%s-%s-%s-%02d" % (escape_lemma(lemma), synset.part_of_speech.value,
                 synset_key(synset.id), idx),
             synset=synset.id,
             n=n,
             sense_key=None))
     else:
         n = 0
-        wn_source.add_entry(LexicalEntry(
-            "ewn-%s-%s" % (escape_lemma(lemma), synset.part_of_speech),
-            Lemma(lemma, synset.part_of_speech),
-            senses=[
-                Sense(
-                    id="ewn-%s-%s-%s-%02d" % (escape_lemma(lemma), synset.part_of_speech,
+        entry = LexicalEntry(
+            "ewn-%s-%s" % (escape_lemma(lemma), synset.part_of_speech.value))
+        entry.set_lemma(Lemma(lemma, synset.part_of_speech))
+        entry.add_sense( Sense(
+                    id="ewn-%s-%s-%s-%02d" % (escape_lemma(lemma), synset.part_of_speech.value,
                         synset_key(synset.id), idx),
-                    synset=synset.id, n=n, sense_key=None)]))
-    with open("src/wn-%s.xml" % synset.lex_name, "w") as out:
-        wn_synset.to_xml(out, True)
+                    synset=synset.id, n=n, sense_key=None))
+        wn_synset.add_entry(entry)
+        with open("src/wn-%s.xml" % synset.lex_name, "w") as out:
+            wn_synset.to_xml(out, True)
 
 def change_sense_n(wn, entry, sense_id, new_n):
     """Change the position of a sense within an entry (changes only this sense)"""
